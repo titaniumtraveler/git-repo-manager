44768577fb48bf3d269cb60ffb13d07251222b41
Jon Heinritz <jon.heinritz@protonmail.com>
2025-04-11 06:34:25 +0200

    feat: impl RepoManifest

--- src/cli/info/schema.rs
+++ src/cli/info/schema.rs
@@ -1,15 +1,20 @@
-use crate::{schemas::config::Config, utils};
+use crate::{
+    schemas::{config::Config, repo_manifest::RepoManifest},
+    utils,
+};
 use clap::Subcommand;
 
 #[derive(Debug, Subcommand, Clone)]
 pub enum Schema {
     Config,
+    RepoManifest,
 }
 
 impl Schema {
     pub fn run(self) -> anyhow::Result<()> {
         match self {
             Schema::Config => utils::write_schema_to_stdout::<Config>(),
+            Schema::RepoManifest => utils::write_schema_to_stdout::<RepoManifest>(),
         }
     }
 }
--- src/schemas.rs
+++ src/schemas.rs
@@ -1,4 +1,5 @@
 pub mod config;
+pub mod repo_manifest;
 pub(crate) mod utils;
 
 pub mod directories;
